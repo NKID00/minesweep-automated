3c866dbed34f356d542a198f4484f0a1e8c09f78
NKID00 <this@nkid00.name>
2024-10-27 22:21:25 +0800

    refactor: remove tauri

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ mod app;
 
 use app::*;
 use leptos::*;
+use wasm_bindgen::prelude::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| {
+    let mount_point: web_sys::HtmlElement = document()
+        .get_elements_by_tag_name("main")
+        .item(0)
+        .expect("mount point not found")
+        .dyn_into()
+        .unwrap();
+    mount_point.replace_children_with_node_0();
+    mount_to(mount_point, || {
         view! {
             <App/>
         }
